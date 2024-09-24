e20605ae81aefa8a09015031ed1691412e47a189
takpap <takpap@foxmail.com>
2024-09-25 01:31:35 +0800

    feat: add report config

--- src/views/manage/user/index.vue
+++ src/views/manage/user/index.vue
@@ -21,7 +21,7 @@ const { columns, filteredColumns, data, loading, pagination, getData, searchPara
   apiFn: fetchGetUserList,
   apiParams: {
     current: 1,
-    size: 10,
+    size: 100,
     // if you want to use the searchParams in Form, you need to define the following properties, and the value is null
     // the value can not be undefined, otherwise the property in Form will not be reactive
     status: null,
